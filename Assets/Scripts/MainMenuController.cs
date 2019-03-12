c2895b8b49d4072fc318bbbe91ae07b32b6613c0
Heeyeong Kim <gldud9833@gmail.com>
2019-03-11 21:04:56 -0400

    Added MainMenu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // What happens when I click the start button?
+    public void StartGame() 
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+
+    // What happens when I click the quit button?
+    public void QuitGame()
+    {
+        // Preprocessor Directives
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+             Application.Quit();
+        #endif
+    }
+}
