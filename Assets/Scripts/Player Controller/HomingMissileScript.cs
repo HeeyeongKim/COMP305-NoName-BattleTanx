ae08577c1b41ad36caf28f343264bf24dbd3d765
Hyojin Kim <akumada54@gmail.com>
2019-03-12 14:49:02 -0400

    Fixed error - Stage 3: collider with barriers

--- Assets/Scripts/Player Controller/HomingMissileScript.cs
+++ Assets/Scripts/Player Controller/HomingMissileScript.cs
@@ -13,26 +13,59 @@ public class HomingMissileScript : MonoBehaviour
 
     Rigidbody2D rb;
 
+    public float bulletSpeed;
+    public float bulletDamage;
+    public float maxTimeAlive;
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.Find("targetWood");
         rb = GetComponent<Rigidbody2D>();
+
+        StartCoroutine(SelfDestruct());
+    }
+
+    void FixedUpdate()
+    {
+        this.transform.Translate(Vector2.up * bulletSpeed * Time.deltaTime);    
+    }
+
+    IEnumerator SelfDestruct()
+    {
+        yield return new WaitForSeconds(maxTimeAlive);
+        Destroy(this.gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("OnTriggerEnter2D method");
+
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("targetWood!!");
+
+            //Destroy(other.gameObject); // this destroys the enemy
+            //Destroy(gameObject); // this destroys the bullet
+
+            //Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+            Destroy(target);
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("Update method");
+    //void Update()
+    //{
+        //Debug.Log("Update method");
 
-        dir = (target.transform.position - transform.position).normalized;
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        rotateToTarget = Quaternion.Slerp(transform.rotation, rotateToTarget,
-            Time.deltaTime * rotationSpeed);
-        rb.velocity = new Vector2(dir.x*2, dir.y*2);
-    }
-
+        //dir = (target.transform.position - transform.position).normalized;
+        //float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        //rotateToTarget = Quaternion.Slerp(transform.rotation, rotateToTarget,
+        //    Time.deltaTime * rotationSpeed);
+        //rb.velocity = new Vector2(dir.x*2, dir.y*2);
+    //}
+/** 
     void OnTriggerEnter2D(Collider2D col)
     {
         Debug.Log("OnTriggerEnter2D method");
@@ -41,4 +74,6 @@ public class HomingMissileScript : MonoBehaviour
         Destroy(this.gameObject);
         Destroy(target);
     }
+*/
+
 }
