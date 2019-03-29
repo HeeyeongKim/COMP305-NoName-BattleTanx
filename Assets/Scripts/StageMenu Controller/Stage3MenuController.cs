f65e6174bb18956c132bc04ef73bfeb413fdb78b
Hyojin Kim <akumada54@gmail.com>
2019-03-29 17:01:24 -0400

    Edited Stage 3: added enemies

---
+++ Assets/Scripts/StageMenu Controller/Stage3MenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage3MenuController : MonoBehaviour
+{
+    public void NextGame()
+    {
+        SceneManager.LoadScene("Boss");
+    }
+
+    // What happens when I click the quit button?
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+}
