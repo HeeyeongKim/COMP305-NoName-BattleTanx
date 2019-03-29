def07ce694bd86a8ca36d2d24ccdc0d54bfdfebc
Heeyeong Kim <gldud9833@gmail.com>
2019-03-29 14:58:51 -0400

    Tutorial backup

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popUps;
+    private int popUpIndex;
+    //public GameObject wood;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            if (i == popUpIndex){
+                popUps[popUpIndex].SetActive(true);
+            }
+            else{
+                popUps[popUpIndex].SetActive(false);
+            }
+        }        
+
+        if(popUpIndex == 0){
+            if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)){
+                popUpIndex++;
+            }
+        } else if(popUpIndex == 1){
+            if(Input.GetKey(KeyCode.W)){
+                popUpIndex++;
+            }
+        } else if(popUpIndex == 2){
+            if(Input.GetMouseButtonDown(0)){
+                popUpIndex++;
+            }
+        }
+    }
+}
