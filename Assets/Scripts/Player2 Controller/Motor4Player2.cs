c899f2e33a9e7218b97c6f6d91b9b3ad18b36fa0
suntek <ost925@gmail.com>
2019-03-30 12:55:55 -0400

    Modify the game to be played in the multiplayer way
    Modify the game to be played in the multiplayer way

---
+++ Assets/Scripts/Player2 Controller/Motor4Player2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Motor4Player2 : MonoBehaviour
+{
+    //public int health = 3;
+    public float moveSpeed = 1;
+    public float rotateSpeed = 12;
+    public GameObject turret;
+    //public Text healthText;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        //healthText.text = "Health: " + health.ToString();
+    }
+
+    void FixedUpdate()
+    {
+        float moveVector = Input.GetAxis("Vertical4Player2");
+        float rotateVector = Input.GetAxis("Horizontal4Player2");
+
+        this.transform.Translate(0f, moveVector * moveSpeed * Time.deltaTime, 0f);
+        this.transform.Rotate(0f, 0f, rotateVector * (rotateSpeed * 10) * Time.deltaTime);
+    }
+
+    void Update()
+    {
+        TurretRotation();
+    }
+
+    void TurretRotation()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(mousePosition.x - turret.transform.position.x, mousePosition.y - turret.transform.position.y);
+
+        turret.transform.up = direction;
+    }
+
+}
---
+++ Assets/Scripts/Player2 Controller/Weapon4Player2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon4Player2 : MonoBehaviour
+{
+
+    public float fireRate = 3;
+
+    float timeUntilFire = 0;
+
+    public Transform firePoint;
+    public GameObject bullet;
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2") && Time.time > timeUntilFire)
+        {
+            timeUntilFire = Time.time + 1 / fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, firePoint.position, firePoint.rotation);
+    }
+}
