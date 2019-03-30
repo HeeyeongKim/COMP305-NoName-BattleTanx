12a730937e7c4a7472b53e054f3899bb5bb3d98f
Heeyeong Kim <gldud9833@gmail.com>
2019-03-30 14:23:36 -0400

    Added score

---
+++ Assets/Scripts/StageMenu Controller/GameOverController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour
+{
+     public void StartGame()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+
+   // What happens when I click the quit button?
+    public void QuitGame()
+    {
+        // Preprocessor Directives
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+             Application.Quit();
+        #endif
+    }
+}
--- Assets/Scripts/StageMenu Controller/Stage3MenuController.cs
+++ Assets/Scripts/StageMenu Controller/Stage3MenuController.cs
@@ -2,9 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Stage3MenuController : MonoBehaviour
 {
+    private Animator anim; // board animation after level completed
+    public Text txtScore;
+    public static int score;
+
+    void Start () 
+    {
+        anim = (GameObject.FindWithTag("levelCompleted")).GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        txtScore.text = "SCORE: " + score.ToString();
+
+        if (score >= 140)
+        {
+            anim.SetBool("isFinished", true);
+        }
+    }
+
     public void NextGame()
     {
         SceneManager.LoadScene("Boss");
@@ -13,6 +33,11 @@ public class Stage3MenuController : MonoBehaviour
     // What happens when I click the quit button?
     public void QuitGame()
     {
-        SceneManager.LoadScene("GameOver");
+        // Preprocessor Directives
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+             Application.Quit();
+        #endif
     }
 }
