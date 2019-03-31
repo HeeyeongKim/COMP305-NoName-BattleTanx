97feee030a082f9fe325587dced5353e159d5f48
Din Lanh <langdinhieu@gmail.com>
2019-03-31 15:40:15 -0400

    fixed sound

--- Assets/Scripts/StageMenu Controller/SoundManagerScript.cs
+++ Assets/Scripts/StageMenu Controller/SoundManagerScript.cs
@@ -6,6 +6,7 @@ public class SoundManagerScript : MonoBehaviour
 {
     public static AudioClip enemyExplosionSound; // explosion sound for enemy
     public static AudioClip woodExplosionSound; // explosion sound for wood
+    public static AudioClip shootingSound; // explosion sound for wood
     static AudioSource audioSrc;
 
     // Start is called before the first frame update
@@ -13,11 +14,24 @@ public class SoundManagerScript : MonoBehaviour
     {
         enemyExplosionSound = Resources.Load<AudioClip>("EnemyExplosion");
         woodExplosionSound = Resources.Load<AudioClip>("WoodExplosion");
+        shootingSound = Resources.Load<AudioClip>("Shoot");
         audioSrc = GetComponent<AudioSource>();
     }
 
     // Sounds
-    public static void playSound(){
+    public static void playSound(string sound){
+        switch (sound)
+      {
+          case "EnemyExplosion":
+              audioSrc.PlayOneShot(enemyExplosionSound);
+              break;
+          case "WoodExplosion":
+              audioSrc.PlayOneShot(woodExplosionSound);
+              break;
+            case "Shoot":
+              audioSrc.PlayOneShot(shootingSound);
+              break;
+      }
         audioSrc.PlayOneShot(enemyExplosionSound);
     }
 
